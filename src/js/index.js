63e8cb0d68a5b6d13dc6ad3670134bc2c5c640e6
kimmoonju-102 <kimmun3549@naver.com>
2023-12-17 15:32:41 +0900

    refactor : 재사용 함수로 분리 및 파일 분리

--- src/js/index.js
+++ src/js/index.js
@@ -17,18 +17,9 @@
 // - [x] 품절 버튼을 추가한다.
 // - [x] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
 // - [x] 클릭이벤트에서 가장 가까운 li태그롸 class속성 값에 sold-out을 추가한다.
+import { $ } from "./utils/dom.js";
+import store from "./store/index.js";
 
-// $ -> html element를 가져올때 사용함 (재사용)
-const $ = (selector) => document.querySelector(selector);
-
-const store = {
-  setLocalStorage(menu) {
-    localStorage.setItem("menu", JSON.stringify(menu));
-  },
-  getLocalStorage() {
-    return JSON.parse(localStorage.getItem("menu"));
-  },
-};
 function App() {
   //  상태 = 변할 수 있는 데이터 : 메뉴명 상태관리
   // menu를 배열로 초기화해서 데이터 관리
@@ -46,9 +37,10 @@ function App() {
       this.menu = store.getLocalStorage();
     }
     render();
+    initEventListeners();
   };
   const updateMenuCount = () => {
-    const menuCount = $("#menu-list").querySelectorAll("li").length;
+    const menuCount = this.menu[this.currentCategory].length;
     $(".menu-count").innerText = `총 ${menuCount} 개`;
   };
 
@@ -105,7 +97,7 @@ function App() {
     const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
     this.menu[this.currentCategory][menuId].name = updatedMenuName;
     store.setLocalStorage(this.menu);
-    $menuName.innerText = updatedMenuName;
+    render();
   };
 
   // 메뉴 삭제 함수
@@ -114,8 +106,7 @@ function App() {
       const menuId = e.target.closest("li").dataset.menuId;
       this.menu[this.currentCategory].splice(menuId, 1);
       e.target.closest("li").remove();
-      store.setLocalStorage(this.menu);
-      updateMenuCount();
+      render();
     }
   };
 
@@ -127,52 +118,54 @@ function App() {
     render();
   };
 
-  $("#menu-list").addEventListener("click", (e) => {
-    if (e.target.classList.contains("menu-edit-button")) {
-      updateMenuName(e);
-      return;
-    }
+  const initEventListeners = () => {
+    $("#menu-list").addEventListener("click", (e) => {
+      if (e.target.classList.contains("menu-edit-button")) {
+        updateMenuName(e);
+        return;
+      }
 
-    if (e.target.classList.contains("menu-remove-button")) {
-      removeMenuName(e);
-      return;
-    }
+      if (e.target.classList.contains("menu-remove-button")) {
+        removeMenuName(e);
+        return;
+      }
 
-    if (e.target.classList.contains("menu-sold-out-button")) {
-      soldOutMenu(e);
-      return;
-    }
-  });
+      if (e.target.classList.contains("menu-sold-out-button")) {
+        soldOutMenu(e);
+        return;
+      }
+    });
 
-  // form 태그가 자동으로 전송되는 걸 막아준다
-  $("#menu-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
+    // form 태그가 자동으로 전송되는 걸 막아준다
+    $("#menu-form").addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
 
-  // 확인버튼
-  $("#menu-submit-button").addEventListener("click", addMenuName);
+    // 확인버튼
+    $("#menu-submit-button").addEventListener("click", addMenuName);
 
-  // element 찾고, 이벤트 받기
-  $("#menu-name").addEventListener("keypress", (e) => {
-    if (e.key !== "Enter") {
-      return;
-    }
-    addMenuName();
-  });
+    // element 찾고, 이벤트 받기
+    $("#menu-name").addEventListener("keypress", (e) => {
+      if (e.key !== "Enter") {
+        return;
+      }
+      addMenuName();
+    });
 
-  // 카테고리별로 관리하기
-  $("nav").addEventListener("click", (e) => {
-    const isCategoryButton = e.target.classList.contains("cafe-category-name");
-    // 예외처리 : 해당 클래스만 찾아서 이벤트 처리
-    if (isCategoryButton) {
-      // 데이터 속성 갖고 오기
-      const categoryName = e.target.dataset.categoryName;
-      this.currentCategory = categoryName;
-      // 카테고리 안에 타이틀 값을 바꿈
-      $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
-      render();
-    }
-  });
+    // 카테고리별로 관리하기
+    $("nav").addEventListener("click", (e) => {
+      const isCategoryButton = e.target.classList.contains("cafe-category-name");
+      // 예외처리 : 해당 클래스만 찾아서 이벤트 처리
+      if (isCategoryButton) {
+        // 데이터 속성 갖고 오기
+        const categoryName = e.target.dataset.categoryName;
+        this.currentCategory = categoryName;
+        // 카테고리 안에 타이틀 값을 바꿈
+        $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
+        render();
+      }
+    });
+  };
 }
 
 const app = new App(); // 생성자 함수로 사용하기 위해 new 키워드를 사용하여 인스턴스를 생성합니다.
---
+++ src/js/utils/dom.js
@@ -0,0 +1,2 @@
+// $ -> html element를 가져올때 사용함 (재사용)
+export const $ = (selector) => document.querySelector(selector);
